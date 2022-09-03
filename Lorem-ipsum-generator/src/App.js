a8873db154c2d77f83e7f55528de9160c47db871
Bhaskar <bhaskarsrivastav24@gmail.com>
2022-09-03 17:06:28 +0530

    Lorem-ipsum-generator created

---
+++ Lorem-ipsum-generator/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("hello world!");
+    let amount = parseInt(count);
+    if(count <= 0){
+      amount = 1;
+    }
+    if(count > 9){
+      amount = 9;
+    }
+    setText(data.slice(0, amount)); //end is not included in slice.
+  }
+
+  return (
+  <section className = "section-center">
+       <h3>Tired of boring lorem ipsum?</h3>
+       <form className = "lorem-form" onSubmit = {handleSubmit}>
+          <label htmlFor = "amount">
+            Paragraphs :
+          </label>
+          <input
+          type = "number"
+          name = "amount"
+          id = "amount"
+          value = {count}
+          onChange = {(e) => setCount(e.target.value)} />
+
+          <button type = "submit" className = "btn">generate</button>
+
+       </form>
+       <article className = "lorem-text">
+          {text.map((item, index) => {
+            return <p key = {index}>{item}</p>
+
+
+          })}
+       </article>
+  </section>
+);
+}
+
+export default App;
